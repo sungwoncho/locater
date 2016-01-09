c17f0c9acc4397ce50089837024b2338932ee3e3
Sung Won Cho <mikeswcho@gmail.com>
2016-01-09 17:16:21 +1100

    Add .find and .any API

--- test/locater_test.js
+++ test/locater_test.js
@@ -3,59 +3,83 @@ var fs = require('fs');
 var locater = require('../lib/locater');
 
 describe("locater", function() {
-  it("matches string and returns locations of results", function() {
-    var input = fs.readFileSync(
-      './test/fixtures/sample_input.txt', {encoding: 'utf8'});
+  describe(".find", function() {
+    it("matches string and returns locations of results", function() {
+      var input = fs.readFileSync(
+        './test/fixtures/sample_input.txt', {encoding: 'utf8'});
 
-    var result = locater('my', input);
+      var result = locater.find('my', input);
 
-    expect(result[0].line).to.equal(1);
-    expect(result[0].cursor).to.equal(4);
-    expect(result[1].line).to.equal(2);
-    expect(result[1].cursor).to.equal(1);
-    expect(result[2].line).to.equal(3);
-    expect(result[2].cursor).to.equal(27);
+      expect(result[0].line).to.equal(1);
+      expect(result[0].cursor).to.equal(4);
+      expect(result[1].line).to.equal(2);
+      expect(result[1].cursor).to.equal(1);
+      expect(result[2].line).to.equal(3);
+      expect(result[2].cursor).to.equal(27);
+    });
+
+    it("matches regex and returns locations of results", function() {
+      var input = fs.readFileSync(
+        './test/fixtures/sample_input.txt', {encoding: 'utf8'});
+
+      var result = locater.find(/[a-zA-Z]{7}\s/g, input);
+
+      expect(result[0].line).to.equal(1);
+      expect(result[0].cursor).to.equal(7);
+      expect(result[1].line).to.equal(1);
+      expect(result[1].cursor).to.equal(27);
+      expect(result[2].line).to.equal(3);
+      expect(result[2].cursor).to.equal(11);
+    });
+
+    it("can match more than once for the same line", function() {
+      var input = "What ain't no country I've ever heard of. They speak English in What?";
+      var result = locater.find('What', input);
+
+      expect(result[0].line).to.equal(1);
+      expect(result[0].cursor).to.equal(1);
+      expect(result[1].line).to.equal(1);
+      expect(result[1].cursor).to.equal(65);
+    });
+
+    it("returns an empty array when no match is present", function() {
+      var result = locater.find('What', "Zed's dead, baby. Zed's dead.");
+      expect(result).to.have.length(0);
+    });
+
+    it("matches multiple occurences of a string in a line", function() {
+      var result = locater.find('my', 'my mind my soul');
+      expect(result[0].line).to.equal(1);
+      expect(result[0].cursor).to.equal(1);
+      expect(result[1].line).to.equal(1);
+      expect(result[1].cursor).to.equal(9);
+    });
+
+    it("matches multiple occurences of a regex in a line", function() {
+      var result = locater.find(/[a-zA-Z]{6}/g, 'kaskade - 4AM');
+      expect(result.length).to.equal(2);
+    });
   });
 
-  it("matches regex and returns locations of results", function() {
-    var input = fs.readFileSync(
-      './test/fixtures/sample_input.txt', {encoding: 'utf8'});
+  describe(".any", function() {
+    it("returns true if there is at least one match with string", function() {
+      var result = locater.any('hello', 'hello world');
+      expect(result).to.equal(true);
+    });
 
-    var result = locater(/[a-zA-Z]{7}\s/g, input);
+    it("returns false if there is no match with string", function() {
+      var result = locater.any('foo', 'Say what again');
+      expect(result).to.equal(false);
+    });
 
-    expect(result[0].line).to.equal(1);
-    expect(result[0].cursor).to.equal(7);
-    expect(result[1].line).to.equal(1);
-    expect(result[1].cursor).to.equal(27);
-    expect(result[2].line).to.equal(3);
-    expect(result[2].cursor).to.equal(11);
-  });
+    it("returns true if there is at least one match with regex", function() {
+      var result = locater.any(/[a-z]{5}/g, 'hello world');
+      expect(result).to.equal(true);
+    });
 
-  it("can match more than once for the same line", function() {
-    var input = "What ain't no country I've ever heard of. They speak English in What?";
-    var result = locater('What', input);
-
-    expect(result[0].line).to.equal(1);
-    expect(result[0].cursor).to.equal(1);
-    expect(result[1].line).to.equal(1);
-    expect(result[1].cursor).to.equal(65);
-  });
-
-  it("returns an empty array when no match is present", function() {
-    var result = locater('What', "Zed's dead, baby. Zed's dead.");
-    expect(result).to.have.length(0);
-  });
-
-  it("matches multiple occurences of a string in a line", function() {
-    var result = locater('my', 'my mind my soul');
-    expect(result[0].line).to.equal(1);
-    expect(result[0].cursor).to.equal(1);
-    expect(result[1].line).to.equal(1);
-    expect(result[1].cursor).to.equal(9);
-  });
-
-  it("matches multiple occurences of a regex in a line", function() {
-    var result = locater(/[a-zA-Z]{6}/g, 'kaskade - 4AM');
-    expect(result.length).to.equal(2);
+    it("returns false if there is no match with regex", function() {
+      var result = locater.any(/\d+/g, 'hello world');
+      expect(result).to.equal(false);
+    });
   });
 });
