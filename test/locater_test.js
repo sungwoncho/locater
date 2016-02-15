56c52732e2a37c095d58d5f105f5820cdb616e7f
Mark <pagetribemail@gmail.com>
2016-02-15 13:45:54 +1100

    attempt at accepting an array as input

--- test/locater_test.js
+++ test/locater_test.js
@@ -4,6 +4,22 @@ var locater = require('../lib/locater');
 
 describe("locater", function() {
   describe(".find", function() {
+    it("matches array and returns locations of results", function() {
+      var input = fs.readFileSync(
+        './test/fixtures/sample_input.txt', {encoding: 'utf8'});
+
+      var result = locater.find(['my', 'me'], input);
+
+      expect(result[0].line).to.equal(1);
+      expect(result[0].cursor).to.equal(4);
+      expect(result[1].line).to.equal(2);
+      expect(result[1].cursor).to.equal(1);
+      expect(result[2].line).to.equal(2);
+      expect(result[2].cursor).to.equal(16);
+      expect(result[3].line).to.equal(3);
+      expect(result[3].cursor).to.equal(27);
+    });
+
     it("matches string and returns locations of results", function() {
       var input = fs.readFileSync(
         './test/fixtures/sample_input.txt', {encoding: 'utf8'});
@@ -16,6 +32,7 @@ describe("locater", function() {
       expect(result[1].cursor).to.equal(1);
       expect(result[2].line).to.equal(3);
       expect(result[2].cursor).to.equal(27);
+
     });
 
     it("matches regex and returns locations of results", function() {
@@ -56,8 +73,8 @@ describe("locater", function() {
     });
 
     it("matches multiple occurences of a regex in a line", function() {
-      var result = locater.find(/[a-zA-Z]{6}/g, 'kaskade - 4AM');
-      expect(result.length).to.equal(2);
+      var result = locater.find(/[aA]/g, 'kaskade - 4AM');
+      expect(result.length).to.equal(3);
     });
   });
 
